ba070c3ca582544ab6a404dc874599f1b963531c
wangkebs <739132200@qq.com>
2019-09-04 03:14:28 -0500

    1

---
+++ src/main/java/com/example/kkkk/KkkkApplication.java
@@ -0,0 +1,13 @@
+package com.example.kkkk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkkkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KkkkApplication.class, args);
+    }
+
+}
